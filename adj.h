88d870e3f2671b2c4c7e8b1ab99f537fca481a8a
Mario-Hunter <mario.hany.13@gmail.com>
2016-12-19 14:32:00 +0200

    Add files via upload

---
+++ adj.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   adj.h
+ * Author: Mario
+ *
+ * Created on December 1, 2016, 2:13 AM
+ */
+
+#ifndef ADJ_H
+#define	ADJ_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+extern void findAdj(int adj[][2],int i,int j,int width,int height);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADJ_H */
+
